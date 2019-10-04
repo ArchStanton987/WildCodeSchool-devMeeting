afda844661789be6bda4ae92322313a86e72fdd6
Victor Biard <vi.biard@gmail.com>
2019-10-04 10:33:23 +0200

    dernière modif menu burger (ajout html css et js dans burgermenu.js)

--- burger_menu.js
+++ burger_menu.js
@@ -1,5 +1,6 @@
 const burgerMenu = document.getElementById('hamburger');
-
+const burgerButton = document.getElementById('hamburger-button');
+const buttonCloseBurger = document.getElementById('close-burger');
 
 document.getElementById ('hamburger-button').addEventListener("click", openBurger)
 let close = false
@@ -7,11 +8,13 @@ function openBurger(){
     if(!close){
     burgerMenu.style.transition = '0.5s';
     burgerMenu.style.left = '0px';
+    burgerButton.style.display = 'none';
+    buttonCloseBurger.style.display = 'inline-block';
     close = true;
     } else {
     burgerMenu.style.transition = 'O,1s';
-    burgerMenu.style.left = '-500px'
-    close = false
+    burgerMenu.style.left = '-500px';
+    close = false;
     }    
 }
 /*
@@ -28,12 +31,22 @@ const anchorLinks = document.getElementsByClassName('anchorLinks');
             })            
         }     
             
-    }
+    }*/
+
+document.getElementById ('close-burger').addEventListener("click", closeBurger)
+
+function closeBurger(){
+    burgerMenu.style.transition = 'O,1s';
+    burgerMenu.style.left = '-500px';
+    burgerButton.style.display = 'inline-block';
+    buttonCloseBurger.style.display = 'none';
+    close = false   
+}   
         
 
     
 
-    closeBurger(); */
+
 
 
 
